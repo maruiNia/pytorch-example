38ad78a98da44afa3cda7c8868352c4d823230fb
maruiNia <maruinano@gmail.com>
2025-06-27 10:16:29 +0900

    hello 주석 추가

--- mnist/main.py
+++ mnist/main.py
@@ -94,6 +94,7 @@ def main():
                         help='For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
+    # hello
 
     torch.manual_seed(args.seed)
 
